a167dc8e151ec0868e8965eada39da6d3b0b2440
Jake Schnorr <jschnorr@msudenver.edu>
2019-09-22 20:45:07 -0600

    Removed unnecessary description field fro cars.

--- db/migrate/20190923014217_create_cars.rb
+++ db/migrate/20190923014217_create_cars.rb
@@ -1,7 +1,6 @@
 class CreateCars < ActiveRecord::Migration[5.2]
   def change
     create_table :cars do |t|
-      t.string :description
       t.string :make
       t.string :model
       t.string :vinNumber
