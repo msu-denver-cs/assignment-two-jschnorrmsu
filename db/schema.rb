d55d413f070b462895a12340a218056090afdfaa
Jake Schnorr <jschnorr@msudenver.edu>
2019-09-22 19:43:29 -0600

    Generated scaffold 'Car' for cars, migrated db.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_23_013934) do
+ActiveRecord::Schema.define(version: 2019_09_23_014217) do
+
+  create_table "cars", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "parts", force: :cascade do |t|
     t.datetime "created_at", null: false
