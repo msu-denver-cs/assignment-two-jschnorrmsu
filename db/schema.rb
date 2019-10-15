3233a2a184f92eec38eb35eb1b76b94202c668f9
Jake Schnorr <jschnorr@msudenver.edu>
2019-10-15 12:23:57 -0600

    Migrate DB.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 ActiveRecord::Schema.define(version: 2019_10_10_194227) do
 
   create_table "cars", force: :cascade do |t|
+    t.string "make"
+    t.string "model"
+    t.string "vinNumber"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -23,13 +26,13 @@ ActiveRecord::Schema.define(version: 2019_10_10_194227) do
   end
 
   create_table "makes", force: :cascade do |t|
-    t.string "name"
-    t.string "country"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "parts", force: :cascade do |t|
+    t.string "car_type"
+    t.string "cars_available"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
