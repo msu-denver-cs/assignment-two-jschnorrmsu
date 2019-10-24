713e561a884b9884991fc01ec30b492a0262ae02
Jake Schnorr <jschnorr@msudenver.edu>
2019-10-24 11:43:24 -0600

    updated routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,28 @@
 Rails.application.routes.draw do
+
+  resources :parts do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :cars do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :makes do
+    collection do
+      get 'search'
+    end
+  end
+
   resources :cars_parts
   resources :makes
   resources :cars
   resources :parts
+
   get 'say/hello'
   get 'say/goodbye'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
