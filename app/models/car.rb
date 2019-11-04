0497f6ca169f61a3166d78ab258d35e3dff7aae9
Jake Schnorr <jschnorr@msudenver.edu>
2019-11-03 22:42:35 -0700

    Added validation for car model.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,4 +1,11 @@
 class Car < ApplicationRecord
   belongs_to :make
   has_and_belongs_to_many :parts
+
+  #Validations
+  validates :make, presence:true
+  validates :model, presence:true
+  validates :country, presence: true
+  validates :vin, numericality: {greater_than_or_equal_to: 1}
+
 end
